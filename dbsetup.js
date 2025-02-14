dcf516695176f71a413c0cb3d76a680ae20813fa
not-damien <93546644+not-damien@users.noreply.github.com>
2025-02-13 17:36:06 -0800

    fix error

--- dbsetup.js
+++ dbsetup.js
@@ -1,4 +1,4 @@
-const sqlite3 = require('sqlite3').verbose
+const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./reddit.db')
 db.serialize(()=> {
     db.run(`CREATE TABLE IF NOT EXISTS users (userId INTEGER PRIMARY KEY AUTOINCREMENT, 
