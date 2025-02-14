78ee0ffbc9ec50ac4b2d734dda55961bf4d0bfd3
= <=>
2025-02-13 17:28:45 -0800

    fixing bugs

---
+++ dbsetup.js
@@ -0,0 +1,9 @@
+const sqlite3 = require('sqlite3').verbose
+const db = new sqlite3.Database('./reddit.db')
+db.serialize(()=> {
+    db.run(`CREATE TABLE IF NOT EXISTS users (userId INTEGER PRIMARY KEY AUTOINCREMENT, 
+        userName TEXT,
+        password TEXT,
+        email TEXT
+        )`); 
+}) 
