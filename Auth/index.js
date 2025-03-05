7085f7439a39fb12e15fd461bd93ec6b59f43195
Dishita Soni <dishitasoni@dismac.local>
2025-03-04 17:10:43 -0800

    added use cors and connected auth to frontend

--- Auth/index.js
+++ Auth/index.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const authRoutes = require('./Routes/auth');
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./Routes/auth");
 
+const cors = require("cors");
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
-app.use(bodyParser.json()); 
-app.use('/auth', authRoutes);
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/auth", authRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the JWT Auth');
+app.get("/", (req, res) => {
+  res.send("Welcome to the JWT Auth");
 });
 
 app.listen(PORT, () => {
