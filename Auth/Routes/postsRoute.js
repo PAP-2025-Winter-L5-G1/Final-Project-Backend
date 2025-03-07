53526f75ac132d5ba258cb127a429ff16bc8f8e9
Dishita <dishita@outlook.com>
2025-03-07 14:51:12 -0800

    merging into frontend

--- Auth/Routes/postsRoute.js
+++ Auth/Routes/postsRoute.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const { createPost, getPosts } = require("../posts");
 
 router.post("/newpost", async (req, res) => {
-  const { postHeader, postContent, communityId, userId, postDate, token } =
-    req.body;
-  createPost(communityId, userId, postContent, postHeader, postDate);
+  const { postHeader, postContent, communityId, postDate, token } = req.body;
+  const id = jwt.verify(token, "your-secret-key");
+  console.log(id);
+  createPost(communityId, id.userId, postContent, postHeader, postDate);
   res.status(200).json({ token });
 });
 
 router.get("/getposts/:communityId", async (req, res) => {
+  console.log(req.params.communityId);
   res.status(200).json(getPosts(req.params.communityId));
 });
 
